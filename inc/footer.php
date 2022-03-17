d07d7dc7140cccc40399023766b3ee725816a07b
KDimla <201980014@psu.palawan.edu.ph>
2022-03-17 18:15:53 +0800

    Changes Made

--- inc/footer.php
+++ inc/footer.php
@@ -1,11 +1,11 @@
 <footer class="mastfoot mt-auto" style="text-align: center;">
     <div class="inner">
       <p>Created By: <br> 
-      <a href="https://github.com/JayrahBautista">JayrahBautista</a>
+      <a href="https://github.com/KDimla">Kurt Dimla</a>
       <br>
-      <a href="https://github.com/danmauie">DanMauieDuenas</a>
+      <a href="https://github.com/JLouEncinas">Jonny Lou Encinas</a>
       <br>
-      <a href="https://github.com/elmerrubiojr">ElmerRubio</a>
+      <a href="https://github.com/judvelasco29">Judy Mea Velasco</a>
       <br>BSIT3 - B1
       </p>
     </div>
