ba2be677310c3648a1ca9eebec8939bbd37df0e0
KDimla <201980014@psu.palawan.edu.ph>
2022-03-17 16:45:10 +0800

    Dimla_Kurt

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+	define('ROOT_URL', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6479631');
+	define('DB_PASS', 'mKYgz1u7WM');
+	define('DB_NAME', 'sql6479631');
+?>
